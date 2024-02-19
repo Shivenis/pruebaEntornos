60aa7942e35b02a85afa2742aece22e66a39e688
DCorona <daniel.corona.aguilar.alu@iesfernandoaguilar.es>
2024-02-19 15:03:15 +0100

    Segundo commit

--- HolaGit.java
+++ HolaGit.java
@@ -1,6 +1,6 @@
 public class HolaGit{
 	public static void main(String[] args){
 		System.out.println("Hola mundo");
-		System.out.println("Nueva linea agregada"): 
+		System.out.println("Nueva linea agregada2"): 
 	}
 }
